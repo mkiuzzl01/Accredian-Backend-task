716c805ebc031263e25bc528d006b8f9fc8e3a3c
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-07-15 00:57:06 +0600

    this server is ready to deploy

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(express.json());
 
 // MySQL connection pool setup
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'refer_info_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Test the database connection
@@ -30,7 +30,6 @@ const pool = mysql.createPool({
 
 // post refer info to database
 app.post("/refer_info", async (req, res) => {
-    console.log(req.body);
     const { name, email, refereeName, refereeEmail } = req.body;
   
     try {
@@ -60,6 +59,10 @@ app.get("/refer-info", async (req, res) => {
 });
 
 // Start the server
+app.get('/', async(req,res)=>{
+  res.send("Learn and Earn Server is running")
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
